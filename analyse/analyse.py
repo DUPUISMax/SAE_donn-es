b1b382b19b0bf130a53065db66f1bbe1af9190e9
DUPUISMax <dupuis.camille2005@gmail.com>
2025-01-20 19:01:21 +0100

    m7

--- analyse/analyse.py
+++ analyse/analyse.py
@@ -1,38 +1,31 @@
 from module import * 
+
 import json
-data=lecture("données/Voiture/17.01.20h.json") # mettre le nom du fichier à ouvrir
-temps=lecture("données/Temps/17.01.20h.json")
-L1=[]  
-for Antigone in data: # mettre le nom du parking à analyser
-    L1.append(Antigone["Antigone"]["place"]) # analyse d'une donnée
-print(L1)
 
-# for i in range(len(temps)):
-#     tps.append(temps[i])
-# if len(L1)!=len(temps): # vérification de la cohérence des données
-#     print("erreur")
+# Charger les données du fichier JSON
+with open('données/Voiture/17.01.20h.json', 'r') as file:
+    data = json.load(file)
+with open('données/Temps/17.01.20h.json', 'r') as file:
+    data2 = json.load(file)
+# Initialiser un dictionnaire pour stocker les places par parking
+parkings = {}
+print(data2[])
+# Récupérer les places de chaque parking
+for entry in data:
+    for parking_name, parking_info in entry.items():
+        if parking_name not in parkings:
+            parkings[parking_name] = []
+        parkings[parking_name].append(parking_info['place'])
 
-# print(L1)
-# print(temps)
+# Afficher les places pour chaque parking
+for parking_name, places in parkings.items():
+    courbe(data2,places, parking_name)
 
-# totalplace=lecture("données/Voiture/placeTotal/placeTotalVoiture.json") # mettre le chemin du fichier à ouvrir (placeTotal)
-# totalparking=0
 
-# for Antigone in totalplace:
-#     totalparking+=Antigone["Antigone"]["placetotal"]
 
-# print(courbe(temps,L1,"place libre"))
-# print(pourcentage(L1,totalparking))
-# print(courbe(temps,pourcentage(L1,totalparking),"pourcentage de place libre"))
-tot=(len(temps))
-L1=[]
-for cle in data:
-    for key in cle.keys():
-        print(key)
-        print(cle.keys())
-        if key == cle.keys():   
-                L1.append(key[data]["place"])
-print(L1)
+
+
+
         
     
     
