e211417f56a2cd87e7e146ce4546a3be15edc931
DUPUISMax <dupuis.camille2005@gmail.com>
2025-01-20 19:52:21 +0100

    m1

--- analyse/analyse.py
+++ analyse/analyse.py
@@ -4,12 +4,15 @@ import json
 
 # Charger les données du fichier JSON
 with open('données/Voiture/17.01.20h.json', 'r') as file:
+with open('données/Voiture/test2.json', 'r') as file:
     data = json.load(file)
 with open('données/Temps/17.01.20h.json', 'r') as file:
+with open('données/Temps/test2.json', 'r') as file:
     data2 = json.load(file)
 # Initialiser un dictionnaire pour stocker les places par parking
 parkings = {}
 print(data2[])
+
 # Récupérer les places de chaque parking
 for entry in data:
     for parking_name, parking_info in entry.items():
@@ -20,6 +23,7 @@ for entry in data:
 # Afficher les places pour chaque parking
 for parking_name, places in parkings.items():
     courbe(data2,places, parking_name)
+    print(courbe(data2,places, parking_name))
 
 
 
