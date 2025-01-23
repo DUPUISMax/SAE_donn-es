95b1fa7b2f1e9bc245fd1ff2aeb81ded2d7fe276
maxibigmac1234 <145214468+maxibigmac1234@users.noreply.github.com>
2025-01-23 14:54:58 +0100

    mis à jour

--- analyse/analysecroisée.py
+++ analyse/analysecroisée.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 import json
 from module import *
 
-with open('données/Vélo/22.01.22h.json', 'r') as file:
+with open('données/Vélo/22.01.8h00.json', 'r') as file:
     data_velo = json.load(file)
 
-with open('données/Voiture/2.01.22h.json', 'r') as file:
+with open('données/Voiture/22.01.8h00.json', 'r') as file:
     data_voiture = json.load(file)
 
 tableauvoiture = chargevoiture(data_voiture)
--- analyse/module.py
+++ analyse/module.py
@@ -150,11 +150,11 @@ def MEFcroisee (t1,t2):
     fig.colorbar(cax)
     velo = []
     for entry in t1:
-        for parking_name, parking_info in entry.items():
+        for parking_name in entry.items():
             velo.append(parking_name)
     voiture = []
     for entry in t2:
-        for parking_name, parking_info in entry.items():
+        for parking_name in entry.items():
             voiture.append(parking_name)
     cursor = mplcursors.cursor(cax, hover=True)
     cursor.connect("add", lambda sel: sel.annotation.set_text(f"{voiture[int(sel.target[0])]} vs {velo[int(sel.target[1])]}\nCorrélation: {tableau[int(sel.target[0])][int(sel.target[1])]}"))
